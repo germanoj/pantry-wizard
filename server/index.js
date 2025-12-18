1aab428dd00aee574e1183660e52aeeba22692db
Hayley McVicar <hrmcvicar@gmail.com>
2025-12-17 20:26:38 -0800

    fixed port, index.js, and server to run db

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 import express from "express";
+
 const app = express();
 export default app;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
