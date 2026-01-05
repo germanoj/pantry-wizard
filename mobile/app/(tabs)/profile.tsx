cb1a6d57454dc3426d2a4792b9eb680319b60605
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-05 09:51:15 -0800

    updated navbar icons

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,14 +1,16 @@
-import { Text, View, StyleSheet } from 'react-native';
-/*if logged in do not see this page*/
+import { Text, View, StyleSheet } from "react-native";
 
 export default function ProfilePage() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome back, user!</Text>
-            <Text>Here are the recipes you&apos;ve tried in the past:</Text>
-            <Text>Ready to try a new recipe? Visit the wizard & let&apos;s get cookin&apos;!</Text> 
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome back, user!</Text>
+      <Text>Here are the recipes you&apos;ve tried in the past:</Text>
+      <Text>
+        Ready to try a new recipe? Visit the wizard & let&apos;s get
+        cookin&apos;!
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -17,6 +19,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#fff",
+    padding: 24,
+    gap: 10,
   },
   title: {
     fontSize: 24,
