5dc118108088ceee4506becfa7ddbdf3e0f65ed5
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-06 10:06:35 -0800

    created register page and added dummy data to practice

--- mobile/app/(tabs)/login.tsx
+++ mobile/app/(tabs)/login.tsx
@@ -1,4 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
+
+import {Link} from 'expo-router'
 /*if logged in do not need to see this page*/
 
 export default function LoginPage() {
@@ -7,7 +9,7 @@ export default function LoginPage() {
             <Text style={styles.title}>Welcome back!</Text>
             <Text>Login to check your fave recipes</Text> {/*add login form here*/}
             <Text style={styles.title}>First time using Pantry Wizard?</Text> 
-            <Text>Click here to make an account to save all your unique recipes!</Text> {/*add register redirect/form here*/}
+            <Text>Click <Link style={styles.link} href="/register">here</Link> to make an account to save all your unique recipes!</Text> {/*add register redirect/form here*/}
         </View>
     )
 }
@@ -23,4 +25,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "600",
   },
+  link: {color: "blue"}
 });
