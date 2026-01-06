f9916dc323d59b58ac247a2c6749b73770bb463e
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-05 20:58:28 -0800

    added buttons on home page for nav

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { router } from "expo-router";
+
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Pantry Wizard 🧙‍♂️</Text>
+    <Button
+        title="Login to your account"
+        onPress={() => router.push("/login")}
+      />
+     <Button
+        title="Or visit the wizard as a guest"
+        onPress={() => router.push("/chatBot")}
+      />
     </View>
   );
 }
