bc2bb92dc934030d2feea0ffaed519c5bb7cb519
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-08 21:31:52 -0800

    added themed colors and fonts to new assests/fonts and src/theme folders

--- mobile/app/(tabs)/loginReg.tsx
+++ mobile/app/(tabs)/loginReg.tsx
@@ -1,15 +1,16 @@
 import { Text, View, StyleSheet } from 'react-native';
-
+import { useTheme } from '@/src/theme/usetheme';
 import {Link} from 'expo-router'
 /*if logged in do not need to see this page*/
 
 export default function LoginPage() {
+  const theme = useTheme();
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome back!</Text>
-            <Text> <Link style={styles.link} href="/login">Login</Link>to check your fave recipes</Text> {/*add login form here*/}
-            <Text style={styles.title}>First time using Pantry Wizard?</Text> 
-            <Text>Click <Link style={styles.link} href="/register">here</Link> to make an account to save all your unique recipes!</Text> {/*add register redirect/form here*/}
+        <View style={[styles.container, {backgroundColor: theme.background}]}>
+            <Text style={[styles.title, {color: theme.text}]}>Welcome back!</Text>
+            <Text style={{color: theme.text}}> <Link style={styles.link} href="/login">Login </Link>to check your fave recipes</Text> {/*add login form here*/}
+            <Text style={[styles.title, {color: theme.text}]}>First time using Pantry Wizard?</Text> 
+            <Text style={{color: theme.text}}>Click <Link style={styles.link} href="/register">here</Link> to make an account to save all your unique recipes!</Text> {/*add register redirect/form here*/}
         </View>
     )
 }
@@ -19,11 +20,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
     fontWeight: "600",
   },
-  link: {color: "blue"}
+  link: {color: "#FFD36A"}
 });
