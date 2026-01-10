9781b23e01033670b763e6cb14ac89917722575f
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-10 09:15:25 -0800

    added design to profile page

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,14 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import { useTheme } from '@/src/theme/usetheme';
+import { WizardBody, WizardTitle } from '@/src/components/WizardText';
+import { Card } from '@/src/components/Card';
+//import { use } from "react";
 
 export default function ProfilePage() {
+  const theme = useTheme();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome back, user!</Text>
-      <Text>Here are the recipes you&apos;ve tried in the past:</Text>
-      <Text>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
+      <Card>
+      <WizardTitle>Welcome back, user!</WizardTitle>
+      <WizardBody>Here are the recipes you&apos;ve tried in the past:</WizardBody>
+      <WizardBody>
         Ready to try a new recipe? Visit the wizard & let&apos;s get
         cookin&apos;!
-      </Text>
+      </WizardBody>
+      </Card>
     </View>
   );
 }
