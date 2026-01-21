04a1eaeaeaec16a8b98db4f375538629d6da7e00
Hayley McVicar <hrmcvicar@gmail.com>
2026-01-20 19:46:48 -0800

    fixed color on reg and login, profile logged out centered

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -1,8 +1,16 @@
 import { Stack } from "expo-router";
+import { useTheme } from "@/src/theme/usetheme";
 
 export default function AuthLayout() {
+  const theme = useTheme();
   return (
-    <Stack>
+    <Stack 
+      screenOptions={{ 
+        headerStyle: { backgroundColor: theme.surface2 },
+        headerTitleStyle: { color: theme.text },
+        headerTintColor: theme.text, // back button / icons
+      }}
+    >
       <Stack.Screen name="login" options={{ title: "Login" }} />
       <Stack.Screen name="register" options={{ title: "Register" }} />
     </Stack>
